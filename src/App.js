05371d44c25b872e1baf4ac95a69994973f029eb
pacis32 <pacis32@gmail.com>
2020-08-04 23:12:30 -0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import axios from 'axios'
 
 
-axios.all([
-  axios.get('http://anapioficeandfire.com/api/characters/16'),
-  axios.get('http://www.anapioficeandfire.com/api/houses/378'),
-  axios.get(' http://www.anapioficeandfire.com/api/houses/229'),
-  axios.get('  http://www.anapioficeandfire.com/api/houses/17'),
-  axios.get('   http://www.anapioficeandfire.com/api/characters/901'),
-  axios.get('   http://www.anapioficeandfire.com/api/characters/901'),
+const api= axios.create({
+baseURL: `http://anapioficeandfire.com/api/characters/16`
+})
 
-])
-.then(response => {
-  console.log('Date created: ', response[0].data.created_at);
-  console.log('Date created: ', response[1].data.created_at);
-});
+class App extends Component {
 
-function App() {
+constructor(){
+  super();
+ api.get('/').then(res => {
+   console.log(res.data)
+ })
+}
+render(){
   return (
     <div className="App">
-      
+    
     </div>
   );
 }
+}
+  
 
 export default App;
